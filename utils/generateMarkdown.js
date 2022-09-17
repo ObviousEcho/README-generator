4c06836e83c6c77235f402d6be84d39752750356
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-09-17 16:44:18 -0600

    creating template string inside of generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -28,25 +28,59 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  return `
-This application is covered under the ${license} license.
-  `;
+  return `This application is covered under the ${license} license.`;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return `## ${license}
-${renderLicenseLink(license)}
-  `;
+  return `${renderLicenseLink(license)}
+`;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 ${renderLicenseBadge(data.license)}
----
+  
+## Description
+
+${data.description}
+
+## Table of Contents:
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+### ${data.license}
 ${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+I would love to hear from you with feedback, please feel free to email me!  Take a look at my GitHub profile as well.
+[Email: ](${data.email})
+[GitHub: ](${data.github})
 `;
 }
 
